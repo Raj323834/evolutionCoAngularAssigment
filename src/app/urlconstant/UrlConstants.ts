933fa385dc11b8f86fd4f857eb8fe0e81b25aa96
Rahul Raj <rahulraj@dangalgames.com>
2023-01-14 10:46:34 +0530

    updated

--- src/app/urlconstant/UrlConstants.ts
+++ src/app/urlconstant/UrlConstants.ts
@@ -3,7 +3,7 @@ export class UrlConstants {
     public static getIPApiUrl: string = "http://api.ipify.org/?format=json";
 
     public static getData : string = "/public/v2/users";
-    public static post: string = "/public/v2/users";
-    public static delete: string = "/public/v2/users/";
+    public static postData: string = "/public/v2/users";
+    public static deleteData: string = "/public/v2/users/";
 
 }
--- src/app/views/usercomponent/usercomponent.component.ts
+++ src/app/views/usercomponent/usercomponent.component.ts
@@ -10,25 +10,57 @@ import { AppConstants } from 'src/app/urlconstant/AppConstants';
   styleUrls: ['./usercomponent.component.css']
 })
 export class UsercomponentComponent implements OnInit {
+  headers: any;
 
   constructor(private commonServices: CommonservicesService) { }
 
-  private ipAddress:string = ""
+  private ipAddress: string = ""
   ngOnInit(): void {
-    this.getBannerDetails();
+    this.getUserDetails();
     this.commonServices.getIP().subscribe((res: any) => {
       this.ipAddress = (res != null) ? res.ip : AppConstants.DEFAULT_IP;
     });
   }
 
 
+  public items: string[] = [];
+  public id: Number = 0;
+  name: String ="";
+  username: String = "";
+  email: String = "";
 
-
-  getBannerDetails = () => {
+//Read properties of Json object and its values dynamically
+  readJsonDynamically(){
+    this.headers = Object.keys(this.items[0]);
+  }
+  getUserDetails = () => {
     let Authorization = "97f7d472a66171554a84cf51e53577dc07abefa318e9df14ce7b1d8b006359e8";
     let params: URLSearchParams = new URLSearchParams();
     this.commonServices.getAll(UrlConstants.getData, params.toString(), AppConstants.NO_QUERY_PARAM, this.ipAddress, Authorization, '' + Date.now())
       .subscribe(resData => {
-        console.log(resData)})
-      }
+        this.items = resData;
+        this.readJsonDynamically()
+        console.log(resData)
+      })
+  }
+
+  createUserDetails = () => {
+    let Authorization = "97f7d472a66171554a84cf51e53577dc07abefa318e9df14ce7b1d8b006359e8";
+    let params: URLSearchParams = new URLSearchParams();
+    this.commonServices.create(UrlConstants.postData, params.toString(), AppConstants.NO_QUERY_PARAM, this.ipAddress, Authorization, '' + Date.now())
+      .subscribe(resData => {
+        this.getUserDetails();
+        console.log(resData)
+      })
+  }
+
+  deleteUserDetails = () => {
+    let Authorization = "97f7d472a66171554a84cf51e53577dc07abefa318e9df14ce7b1d8b006359e8";
+    let params: URLSearchParams = new URLSearchParams();
+    this.commonServices.getAll(UrlConstants.deleteData, params.toString(), AppConstants.NO_QUERY_PARAM, this.ipAddress, Authorization, '' + Date.now())
+      .subscribe(resData => {
+        this.getUserDetails();
+        console.log(resData)
+      })
+  }
 }
